1b1399a95b33f9dc3bd8a7b8209492ce2a143159
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-03-27 17:21:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,21 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.on('ready', () => {
-  client.user.setGame(` HyperNetWork Discord .`,'http://shyper-network.buycraft.net/');
-  console.log('---------------');
-  console.log('HyperNetWork Bot Is Online')
-  console.log('---------------')
+    console.log('I am ready!');
 });
+
 client.on('message', message => {
-     if (message.content === ".servers") {
-     let embed = new Discord.RichEmbed()
-  .setColor("#0000FF")
-  .addField("**Server: **" , client.guilds.size)
-  message.channel.sendEmbed(embed);
-    }
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
 });
-client.login("process.env.BOT_TOKEN");
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
